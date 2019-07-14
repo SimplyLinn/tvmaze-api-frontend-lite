21d6c29add321ecfe3b3b80b5d7c0faea3dbfc47
Linn Dahlgren <admin@opposite.biz>
2019-07-14 16:26:10 +0200

    Did the about page

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -2,7 +2,12 @@
   <div>
     <h3>About us</h3>
     <p>
-      Todo
+      Okay, so this is the boiled down "to specifications" version of the tv-maze api project. It does EXACTLY what's asked in the assignment,
+      nothing more, nothing less.
+    </p>
+    <p>
+      To see what happens when I step away from the exact specification to show off, <a href="https://tvmaze.opposite.dev">check out the "non-lite" version</a><br/>
+      - Linn
     </p>
   </div>
 </template>
